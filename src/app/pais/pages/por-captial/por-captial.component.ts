ed0e1613e1ed7016e9086c7bd5a0a3a2b1a52616
Angel <angeltonio86@gmail.com>
2022-04-26 23:55:16 -0400

    first commit

---
+++ src/app/pais/pages/por-captial/por-captial.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-por-captial',
+  templateUrl: './por-captial.component.html',
+  styles: [
+  ]
+})
+export class PorCapitalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
